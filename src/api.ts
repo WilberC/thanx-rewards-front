d7eebb1855eec73a59a5d6d2f251c192c13dfcce
Wilber Carrascal <wilber.carrascal@gmail.com>
2025-03-15 12:47:17 -0500

    update-env-var

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    // baseURL: "http://localhost:3000/api/v1",
+    baseURL: apiUrl,
     headers: {
         "Content-Type": "application/json",
     },
