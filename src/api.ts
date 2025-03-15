fcc8e78a09b26df7e63ffaee3ed748150684602c
Wilber Carrascal <wilber.carrascal@gmail.com>
2025-03-15 12:50:06 -0500

    structure-envs-and-readme

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL_V1;
 
 const api = axios.create({
-    // baseURL: "http://localhost:3000/api/v1",
     baseURL: apiUrl,
     headers: {
         "Content-Type": "application/json",
